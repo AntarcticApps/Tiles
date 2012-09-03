3065b78d70e9b16cfdb56e8e380be34a505596e7
Tanner Smith <tanner@tssoftware.net>
2012-09-03 13:50:59 -0400

    Add buttons for up/down sorting.

--- options/js/main.js
+++ options/js/main.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+	var upButton = '<a type="button" class="btn"><i class="icon-arrow-up"></i></a>';
+	var downButton = '<a type="button" class="btn"><i class="icon-arrow-down"></i></a>';
+
+	var buttonPair = '<div class="btn-toolbar"><div class="btn-group">' + upButton + downButton + '</div></div>';
+
+	var controlsCount = $('.controls').size();
+
+	$('.controls').each(function(index) {
+		if (index == controlsCount - 1) {
+			return;
+		}
+
+		if (index == 0) {
+			$(this).append(downButton);
+		} else if (index == controlsCount - 2) {
+			$(this).append(upButton);
+		} else {
+			$(this).append(buttonPair);
+		}
+	});
+
 	$('#sites').submit(function() {
 		var fields = [];
 
