9d04782b2d02c0dc8b11d6c7070f0ce0a5340278
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-09-04 11:58:15 -0400

    Add URL to sites on new tab page.

--- tab_page/js/main.js
+++ tab_page/js/main.js
@@ -47,7 +47,21 @@ function init() {
 function createSite(abbreviation, url) {
 	var site = document.createElement('li');
 
-	site.innerHTML = abbreviation;
+	site.innerHTML = abbreviation + '<span class="url">' + hostname(url) + '</span>';
 
 	return site;
+}
+
+function hostname(url) {
+	var domainRegex = /^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/igm;
+	var a = domainRegex.exec(url);
+	domainRegex.lastIndex = 0;
+
+	var hostname = a[4];
+
+	if (hostname.substring(0, 4) == "www.") {
+		hostname = hostname.substring(4);
+	}
+
+	return hostname;
 }
