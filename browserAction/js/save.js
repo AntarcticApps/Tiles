65eb3805e58f69a2611d9236e207b78742da1a83
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-09-11 22:23:30 -0400

    Add form to save browser action page for entering an abbreviation.

--- browserAction/js/save.js
+++ browserAction/js/save.js
@@ -1,9 +1,19 @@
-chrome.extension.sendMessage({message: "save"}, function(response) {
-	createSite(response.url, null, function(site) {
-		saveSite(site, function() {
-			document.getElementsByTagName('span')[0].innerHTML = "Site saved!";
 
-			chrome.extension.sendMessage({message: "saved"}, function(response) {});
+document.addEventListener('DOMContentLoaded', function() {
+	document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+	init();
+}, false );
+
+function init() {
+	document.getElementById('form').onsubmit = function() {
+		chrome.extension.sendMessage({ message: "save" }, function(response) {
+			createSite(response.url, document.getElementById('abbreviation').value, function(site) {
+				saveSite(site, function() {
+					chrome.extension.sendMessage({ message: "saved" }, function(response) {});
+				});
+			});
 		});
-	});
-});
+
+		return false;
+	}
+}
