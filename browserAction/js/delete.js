930e6e25faaaf89eed8ae38ebc93d163e4e4a7f3
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-09-11 23:43:23 -0400

    Blur submit button on delete popover.

--- browserAction/js/delete.js
+++ browserAction/js/delete.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	init();
 }, false );
 
-
-
 function init() {
+	setTimeout(function() {
+		document.getElementById('submit-btn').blur();
+	}, 100);
+
 	document.getElementById('form').onsubmit = function() {
 		chrome.extension.sendMessage({ message: "delete" }, function(response) {
 			window.close();
