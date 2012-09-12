9e36ca70eb7806ca57bff66511942dba590ed05c
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-09-11 22:55:10 -0400

    Tweak styles and behavior of delete popup.

--- browserAction/js/delete.js
+++ browserAction/js/delete.js
@@ -1,3 +1,9 @@
-chrome.extension.sendMessage({message: "delete"}, function(response) {
-	document.getElementsByTagName('span')[0].innerHTML = "Deleted!";
+
+const WINDOW_CLOSE_TIMEOUT = 750;
+
+chrome.extension.sendMessage({ message: "delete" }, function(response) {
+	document.getElementsByTagName('span')[0].innerHTML = "Removed";
+	setTimeout(function() {
+		window.close();
+	}, WINDOW_CLOSE_TIMEOUT);
 });
