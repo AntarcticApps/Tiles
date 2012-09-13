4a40d8929844b56ae5955db4b10306d0cd6c01a2
Tanner Smith <tanner@tssoftware.net>
2012-09-13 16:27:46 -0400

    Correctly update all browserActions and use the correct URL for saving.
    Switched to new window oriented update system.

--- background.js
+++ background.js
@@ -1,15 +1,14 @@
-var currentURL = null;
-var currentTabID = null;
+var lastFocusedWindowID = null;
 
 chrome.tabs.onActivated.addListener(function(info) {
 	chrome.tabs.get(info.tabId, function(tab) {
-		update(tab);
+		updateTab(tab);
 	});
 });
 
 chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
 	if (tab.active) {
-		update(tab);
+		updateTab(tab);
 	}
 });
 
@@ -18,17 +17,12 @@ chrome.windows.onFocusChanged.addListener(function(windowID) {
 		return;
 	}
 
-	chrome.windows.get(windowID, { populate: true }, function(window) {
-		var tabs = window.tabs;
+	lastFocusedWindowID = windowID;
 
-		if (tabs && window.type == "normal") {
-			for (var i = 0; i < tabs.length; i++) {
-				if (tabs[i].active) {
-					update(tabs[i]);
-					break;
-				}
-			}
-		}
+	chrome.windows.get(windowID, { populate: true }, function(window) {
+		console.log("Last focused window is now ID " + window.id);
+
+		updateWindow(window);
 	});
 });
 
@@ -61,59 +55,79 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	console.log('Message received: ' + request.message);
 
 	if (request.message == "getUrl") {
-		sendResponse({url: currentURL});
+		getFocusedTab(function(tab) {
+			if (tab) {
+				sendResponse({ url: tab.url });
 
-		console.log('Sent URL');
+				console.log('Sent URL – ' + tab.url);
+			}
+		});
 	} else if (request.message == "saved") {
-		setPopup(false, false);
-		changeIcon(true, null);
+		getFocusedTab(function(tab) {
+			if (tab) {
+				setPopup(false, false, tab.id);
+				changeIcon(true, null, tab.id);
 
-		sendResponse({message: "success"});
+				sendResponse({message: "success"});
 
-		console.log('Sent success');
+				console.log('Sent success');
+
+				updateAllWindows();
+			}
+		});
 	} else if (request.message == "delete") {
 		console.log('Deleting....');
 
-		var url;
-		if (request.url == undefined)
-			url = currentURL;
-		else
-			url = request.url;
+		getFocusedTab(function(tab) {
+			if (!tab) {
+				return;
+			}
 
-		deleteSite(url, function() {
-			setPopup(true, false);
-			changeIcon(false, null);
+			function deleteSiteCallback() {
+				if (!request.url) {
+					setPopup(true, false, tab.id);
+					changeIcon(false, null, tab.id);
+				}
 
-			sendResponse({message: "deleted"});
+				sendResponse({message: "deleted"});
 
-			chrome.extension.sendMessage({message: "deleted"}, function(response) { });
+				chrome.extension.sendMessage({message: "deleted"}, function(response) { });
 
-			console.log('Sent deleted');
+				console.log('Sent deleted');
+
+				updateAllWindows();
+			}
+
+			if (request.url == undefined) {
+				console.log("Deleting site " + tab.url);
+				deleteSite(tab.url, deleteSiteCallback);
+			} else {
+				console.log("Deleting site " + request.url);
+				deleteSite(request.url, deleteSiteCallback);
+			}
 		});
 	}
-
-	chrome.windows.getAll({ populate: true }, function(windows) {
-		for (var i = 0; i < windows.length; i++) {
-			var window = windows[i];
-			var tabs = window.tabs;
-
-			if (tabs && window.type == "normal" && window.focused) {
-				for (var j = 0; j < tabs.length; j++) {
-					var tab = tabs[j];
-
-					if (tab.active) {
-						update(tab);
-						break;
-					}
-				}
-			}
-		}
-	});
 
 	return true;
 });
 
-function setPopup(save, error) {
+function getFocusedTab(callback) {
+	chrome.windows.get(lastFocusedWindowID, { populate: true }, function(window) {
+		if (window && window.type != 'normal') {
+			return callback(null);
+		}
+
+		var tabs = window.tabs;
+
+		for (var j = 0; j < tabs.length; j++) {
+			if (tabs[j].active) {
+				return callback(tabs[j]);
+			}
+		}
+	});
+}
+
+function setPopup(save, error, tabID) {
 	var details = {};
 
 	if (error) {
@@ -126,26 +140,53 @@ function setPopup(save, error) {
 		}
 	}
 
-	details.tabId = currentTabID;
+	details.tabId = tabID;
 
 	chrome.browserAction.setPopup(details);
 
-	console.log('Popup is set to ' + details.popup);
+	// console.log('Popup is set to ' + details.popup);
 }
 
-function update(tab) {
-	console.log("Currently on ID: " + tab.id + " which is at " + tab.url);
+function updateWindow(window) {
+	if (window && window.type == 'normal') {
+		var tabs = window.tabs;
 
-	currentTabID = tab.id;
-	currentURL = tab.url;
+		for (var i = 0; i < tabs.length; i++) {
+			if (tabs[i].active) {
+				if (window.focused) {
+					console.log("Window ID " + window.id + " is focused on active tab on ID: " + tabs[i].id + " which is at " + tabs[i].url);
+				} else {
+					console.log("Window ID " + window.id + " is has active tab ID: " + tabs[i].id + " which is at " + tabs[i].url);
+				}
 
-	if (isChromeURL(currentURL)) {
-		setPopup(false, true);
+				updateTab(tabs[i]);
+
+				break;
+			}
+		}
+	}
+}
+
+function updateAllWindows() {
+	chrome.windows.getAll({ populate: true }, function(windows) {
+		console.log('Updating all windows...');
+
+		for (var i = 0; i < windows.length; i++) {
+			updateWindow(windows[i]);
+		}
+	});
+}
+
+function updateTab(tab) {
+	console.log("Updating tab ID: " + tab.id + " which is at " + tab.url);
+
+	if (isChromeURL(tab.url)) {
+		setPopup(false, true, tab.id);
 	} else {
 		siteExists(tab.url, function(exists) {
-			setPopup(!exists, false);
+			setPopup(!exists, false, tab.id);
 
-			changeIcon(exists, null);
+			changeIcon(exists, null, tab.id);
 		});
 	}
 }
@@ -168,7 +209,7 @@ function siteExists(url, callback) {
 	});
 }
 
-function changeIcon(colors, callback) {
+function changeIcon(colors, callback, tabID) {
 	var details = {};
 
 	if (colors) {
@@ -177,9 +218,9 @@ function changeIcon(colors, callback) {
 		details.path = '../icons/icon-bitty-gray.png';
 	}
 
-	details.tabId = currentTabID;
+	console.log("Changing icon for " + tabID + " to " + details.path);
+
+	details.tabId = tabID;
 
 	chrome.browserAction.setIcon(details, callback);
-
-	console.log('Icon is set to ' + details.path);
 }
