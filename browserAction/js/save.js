f0753a6783b1dc8a00406bcd5b730351961d05be
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-09-15 16:23:03 -0400

    Enforce correct capitalization in save popover. Fix submit button text in save popover.

--- browserAction/js/save.js
+++ browserAction/js/save.js
@@ -9,8 +9,19 @@ function init() {
 		document.getElementById('abbreviation').value = makeAbbreviation(getHostname(response.url));
 	});
 
+	function fixAbbreviationCapitalization() {
+		document.getElementById('abbreviation').value = makeAbbreviation(document.getElementById('abbreviation').value);
+	}
+
+	document.getElementById('abbreviation').addEventListener("propertychange", fixAbbreviationCapitalization);
+	document.getElementById('abbreviation').addEventListener("keyup", fixAbbreviationCapitalization);
+	document.getElementById('abbreviation').addEventListener("input", fixAbbreviationCapitalization);
+	document.getElementById('abbreviation').addEventListener("paste", fixAbbreviationCapitalization);
+
+	var submitButton = document.getElementById('submit-btn');
+	submitButton.innerHTML = chrome.i18n.getMessage('popup_add_tile');
+
 	document.getElementById('form').onsubmit = function() {
-		var submitButton = document.getElementById('submit-btn');
 		submitButton.setAttribute('class', submitButton.getAttribute('class') + ' disabled');
 		submitButton.setAttribute('disabled', 'disabled');
 		submitButton.innerHTML = chrome.i18n.getMessage('popup_add_tile_saving');
