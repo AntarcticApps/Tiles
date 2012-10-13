bc8e29eff4f708f15f7613e806d729f8dbfc152e
Tanner Smith <tanner@tssoftware.net>
2012-10-12 22:02:07 -0400

    Fix indentation/spacing of background.js

--- background.js
+++ background.js
@@ -28,53 +28,49 @@ function init() {
 			console.log(fileEntry.toURL());
 
 			fileEntry.createWriter(function(fileWriter) {
+				fileWriter.onwriteend = function(e) {
+					console.log(e);
+					console.log('Write completed.');
+				};
 
-		      fileWriter.onwriteend = function(e) {
-		      	console.log(e);
-		        console.log('Write completed.');
-		      };
+				fileWriter.onerror = function(e) {
+					console.log('Write failed: ' + e.toString());
+				};
 
-		      fileWriter.onerror = function(e) {
-		        console.log('Write failed: ' + e.toString());
-		      };
+				// Create a new Blob and write it to log.txt.
+				var blob = new Blob(['body { display:none; }'], {type: 'text/plain'});
 
-		      // Create a new Blob and write it to log.txt.
-		      var blob = new Blob(['body { display:none; }'], {type: 'text/plain'});
-
-		      fileWriter.write(blob);
-
+				fileWriter.write(blob);
 		    }, errorHandler);
-
 		}, errorHandler);
 	}
 
 	function errorHandler(e) {
-	  var msg = '';
+		var msg = '';
 
-	  switch (e.code) {
-	    case FileError.QUOTA_EXCEEDED_ERR:
-	      msg = 'QUOTA_EXCEEDED_ERR';
-	      break;
-	    case FileError.NOT_FOUND_ERR:
-	      msg = 'NOT_FOUND_ERR';
-	      break;
-	    case FileError.SECURITY_ERR:
-	      msg = 'SECURITY_ERR';
-	      break;
-	    case FileError.INVALID_MODIFICATION_ERR:
-	      msg = 'INVALID_MODIFICATION_ERR';
-	      break;
-	    case FileError.INVALID_STATE_ERR:
-	      msg = 'INVALID_STATE_ERR';
-	      break;
-	    default:
-	      msg = 'Unknown Error';
-	      break;
-	  };
+		switch (e.code) {
+			case FileError.QUOTA_EXCEEDED_ERR:
+				msg = 'QUOTA_EXCEEDED_ERR';
+				break;
+			case FileError.NOT_FOUND_ERR:
+				msg = 'NOT_FOUND_ERR';
+				break;
+			case FileError.SECURITY_ERR:
+				msg = 'SECURITY_ERR';
+				break;
+			case FileError.INVALID_MODIFICATION_ERR:
+				msg = 'INVALID_MODIFICATION_ERR';
+				break;
+			case FileError.INVALID_STATE_ERR:
+				msg = 'INVALID_STATE_ERR';
+				break;
+			default:
+				msg = 'Unknown Error';
+				break;
+		};
 
-	  console.log('Error: ' + msg);
+		console.log('Error: ' + msg);
 	}
-
 
 	window.webkitStorageInfo.requestQuota(PERSISTENT, 1024*1024, function(grantedBytes) {
 		console.log(grantedBytes);
