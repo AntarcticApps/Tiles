b1238bf486f5fefeeccd537ec0bf508032b898af
Tanner Smith <tanner@tssoftware.net>
2012-10-20 18:57:57 -0400

    Fix indentation for errorHandler in files.js

--- shared/js/files.js
+++ shared/js/files.js
@@ -9,30 +9,30 @@ function onInitFileSystem(fs, callback) {
 }
 
 function errorHandler(e) {
-  var msg = '';
+	var msg = '';
 
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'QUOTA_EXCEEDED_ERR';
-      break;
-    case FileError.NOT_FOUND_ERR:
-      msg = 'NOT_FOUND_ERR';
-      break;
-    case FileError.SECURITY_ERR:
-      msg = 'SECURITY_ERR';
-      break;
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'INVALID_MODIFICATION_ERR';
-      break;
-    case FileError.INVALID_STATE_ERR:
-      msg = 'INVALID_STATE_ERR';
-      break;
-    default:
-      msg = 'Unknown Error';
-      break;
-  };
+	switch (e.code) {
+	case FileError.QUOTA_EXCEEDED_ERR:
+		msg = 'QUOTA_EXCEEDED_ERR';
+		break;
+	case FileError.NOT_FOUND_ERR:
+		msg = 'NOT_FOUND_ERR';
+		break;
+	case FileError.SECURITY_ERR:
+		msg = 'SECURITY_ERR';
+		break;
+	case FileError.INVALID_MODIFICATION_ERR:
+		msg = 'INVALID_MODIFICATION_ERR';
+		break;
+	case FileError.INVALID_STATE_ERR:
+		msg = 'INVALID_STATE_ERR';
+		break;
+	default:
+		msg = 'Unknown Error';
+		break;
+	};
 
-  console.error("Error: " + msg);
+	console.error("Error: " + msg);
 }
 
 function initFileSystem(callback) {
