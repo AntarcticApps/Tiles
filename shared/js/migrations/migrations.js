5e65bc00739efee919392667d8dfaf1ef1c310ca
Tanner Smith <tanner@tssoftware.net>
2012-10-21 17:58:20 -0400

    Move into migrations dir. Start 1-2 migration.

--- shared/js/migrations/migrations.js
+++ shared/js/migrations/migrations.js
@@ -8,6 +8,9 @@ function migrateDatabase() {
 		if (storageVersion == 1) {
 			if (extensionVersion.major == 2) {
 				// Migrate from a database from 1.x.x to 2.x.x
+				migrate_1_to_2(function() {
+					console.log("Migration complete.");
+				});
 			}
 		}
 	});
