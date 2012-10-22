014370431f061dd331ce1d3bd18ec5837ffc231d
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-10-22 14:17:35 -0400

    Improve and add new array utility methods.

--- tests/spec/utilities.js
+++ tests/spec/utilities.js
@@ -21,9 +21,24 @@ describe("Utilities", function() {
 });
 
 describe("An array", function() {
+	it("should remove an element at an arbitrary index", function() {
+		var arr = [100, 200, 300, 400];
+		var removed = arr.remove(0);
+		expect(removed).toEqual(100);
+		expect(arr).toEqual([200, 300, 400]);
+	});
+
 	it("should insert an element at an arbitrary index", function() {
 		var arr = [100, 200, 300, 400];
 		arr.insertAtIndex(0, 3);
 		expect(arr).toEqual([100, 200, 300, 0, 400]);
+	});
+
+	it("should swap elements", function() {
+		var arr = [100, 200, 300, 400];
+		var removed = arr.remove(-1);
+		arr.insertAtIndex(removed, 2);
+		expect(removed).toEqual(400);
+		expect(arr).toEqual([100, 200, 400, 300]);
 	});
 });
