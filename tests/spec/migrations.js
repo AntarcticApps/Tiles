f4246e75ca1825861cccae17e56c87fd18f37189
Tanner Smith <tanner@tssoftware.net>
2012-10-22 19:11:57 -0400

    Write a test for migration with no sites.

--- tests/spec/migrations.js
+++ tests/spec/migrations.js
@@ -47,6 +47,64 @@ describe("A migration", function() {
 			ready = false;
 		});
 
+		describe("should migrate with no sites", function() {
+			beforeEach(function() {
+				// Write the v1.x.x database
+				runs(function() {
+					storage.set({
+						"sitesSize": 0,
+					}, function() {
+						ready = true;
+					});
+				});
+
+				waitsFor(function() {
+					return ready;
+				}, "the v1.x.x database to be set up", 500);
+
+				// Migrate!
+				runs(function() {
+					migrate_1_to_2(function() {
+						done = true;
+					});
+				});
+
+				waitsFor(function() {
+					return done;
+				}, "the migration to complete", 500);
+			});
+
+			it("should have a database version of 2", function() {
+				getStorageVersion(function(version) {
+					expect(version).toBe(2);
+				});
+			});
+
+			it("should not contain any bits from the old database", function() {
+				storage.get("sitesSize", function(items) {
+					expect(items).toEqual({});
+				});
+			})
+
+			it("should have no sites", function() {
+				getSitesCount(function(count) {
+					expect(count).toBe(0);
+				});
+			});
+
+			it("should have the next ID be 0", function() {
+				getNextID(function(id) {
+					expect(id).toBe(0);
+				});
+			});
+
+			it("should have an empty sorted site ids", function() {
+				getSortedSiteIDs(function(ids) {
+					expect(ids).toEqual([]);
+				});
+			});
+		});
+
 		describe("should migrate with 1 site", function() {
 			var testSite = {
 				url: "http://www.google.com/",
@@ -89,11 +147,11 @@ describe("A migration", function() {
 
 			it("should not contain any bits from the old database", function() {
 				storage.get("sitesSize", function(items) {
-					expect(items).toBeEmpty();
+					expect(items).toEqual({});
 				});
 
 				storage.get("site-1", function(items) {
-					expect(items).toBeEmpty();
+					expect(items).toEqual({});
 				});
 			})
 
@@ -103,7 +161,7 @@ describe("A migration", function() {
 				});
 			});
 
-			it("should have the correct next ID", function() {
+			it("should have the next ID be 1", function() {
 				getNextID(function(id) {
 					expect(id).toBe(1);
 				});
