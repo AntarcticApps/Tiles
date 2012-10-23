1cf30d4f5244713a432917f656564b34866081bf
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-10-22 21:10:52 -0400

    Add messaging tests.

---
+++ tests/spec/messaging.js
@@ -0,0 +1,57 @@
+describe("Messages", function() {
+	it("can be sent to a single listener", function() {
+		var calledBack = false;
+
+		runs(function() {
+			addMessageListener("test", function() {
+				calledBack = true;
+			});
+
+			emitMessage("test");
+		});
+
+		runs(function() {
+			expect(calledBack).toBe(true);
+		});
+	});
+
+	it("can be sent to multiple listeners", function() {
+		var calledbackCount = 0;
+
+		runs(function() {
+			var inc = function() {
+				calledbackCount++;
+			};
+
+			addMessageListener("test", inc);
+			addMessageListener("test", inc);
+			addMessageListener("test", inc);
+			
+			emitMessage("test");
+		});
+
+		runs(function() {
+			expect(calledbackCount).toBe(3);
+		});
+	});
+
+	it("can be sent with data", function() {
+		var result = "";
+
+		runs(function() {
+			var concat = function(data) {
+				result += data;
+			};
+
+			addMessageListener("test", concat);
+			addMessageListener("test", concat);
+			addMessageListener("test", concat);
+			
+			emitMessage("test", "fighter");
+		});
+
+		runs(function() {
+			expect(result).toMatch("fighterfighterfighter");
+		});
+	});
+});
