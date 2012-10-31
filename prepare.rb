d815e384515f01c3deaaecf06d744fbf69d04a45
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-10-31 10:42:19 -0400

    Remove promo files and git directory in prepared folder.

--- prepare.rb
+++ prepare.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/env ruby
 
+require "eregex"
 require "fileutils"
 
 version = "200"
 prepared_dir = "../Tiles_#{version}"
 
-FileUtils.cp_r("./", "#{prepared_dir}", :verbose => true, :remove_destination => true)
+FileUtils.rm_r("#{prepared_dir}", :verbose => true)
+FileUtils.cp_r("./", "#{prepared_dir}", :verbose => true)
+FileUtils.rm_r(Dir.glob("#{prepared_dir}/**/*.{psd}"), :verbose => true)
+FileUtils.rm_r(["#{prepared_dir}/.git", "#{prepared_dir}/promos", "#{prepared_dir}/.gitignore", "#{prepared_dir}/prepare.rb"], :verbose => true)
 
-# Dir.glob("#{prepared_dir}/**/*.{js}").each do |file|
-# 	new_name = File.basename(file, ".js") + "_" + version + ".js"
-# 	File.rename(file, new_name)
-# 	puts new_name
-# end
+Dir.glob("#{prepared_dir}/**/*.{js}").each do |file|
+	new_name = File.dirname(file) + "/" + File.basename(file, ".js") + "_" + version + ".js"
+	File.rename(file, new_name)
+end
+
+# match = Regexp.escape(".js")
+# replace = Regexp.escape("_#{version}.js")
+
+# exec("find #{prepared_dir} -type f | cat")
