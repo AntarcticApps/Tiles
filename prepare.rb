516115ada030d94dfde48d55427bd90da7af54ff
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-10-31 12:52:43 -0400

    Append version on end of CSS and HTML files.

--- prepare.rb
+++ prepare.rb
@@ -13,12 +13,19 @@ FileUtils.rm_r(Dir.glob("#{prepared_dir}/**/*.{psd}"), :verbose => true)
 FileUtils.rm(Dir.glob("#{prepared_dir}/**/.DS_Store"), :verbose => true)
 FileUtils.rm_r(["#{prepared_dir}/.git", "#{prepared_dir}/promos", "#{prepared_dir}/.gitignore", "#{prepared_dir}/prepare.rb", "#{prepared_dir}/tests"], :verbose => true)
 
-Dir.glob("#{prepared_dir}/**/*.{js}").each do |file|
-	new_name = File.dirname(file) + "/" + File.basename(file, ".js") + "_" + version + ".js"
+Dir.glob("#{prepared_dir}/**/*.{js,html,css}").each do |file|
+	new_name = File.dirname(file) + "/" + File.basename(file, File.extname(file)) + "_" + version + File.extname(file)
 	File.rename(file, new_name)
 end
 
 match = Regexp.escape("\\.js")
 replace = Regexp.escape("_#{version}.js")
+system("find -E #{prepared_dir} -type f -iregex '(.*\.html|.*/manifest\.json)' -exec sed -i '' s/#{match}/#{replace}/g {} +")
 
-exec("find -E #{prepared_dir} -type f -iregex '(.*\.html|.*/manifest\.json)' -exec sed -i '' s/#{match}/#{replace}/g {} +")
+match = Regexp.escape("\\.html")
+replace = Regexp.escape("_#{version}.html")
+system("find -E #{prepared_dir} -type f -iregex '(.*\.html|.*/manifest\.json)' -exec sed -i '' s/#{match}/#{replace}/g {} +")
+
+match = Regexp.escape("\\.css")
+replace = Regexp.escape("_#{version}.css")
+system("find -E #{prepared_dir} -type f -iregex '(.*\.html|.*/manifest\.json)' -exec sed -i '' s/#{match}/#{replace}/g {} +")
