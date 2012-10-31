ace8f2b5db1fab31841aa6919ce0675832f0ae1d
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-10-31 11:17:51 -0400

    Delete .DS_Store files and tests directory.

--- prepare.rb
+++ prepare.rb
@@ -9,7 +9,8 @@ prepared_dir = "../Tiles_#{version}"
 FileUtils.rm_r("#{prepared_dir}", :verbose => true)
 FileUtils.cp_r("./", "#{prepared_dir}", :verbose => true)
 FileUtils.rm_r(Dir.glob("#{prepared_dir}/**/*.{psd}"), :verbose => true)
-FileUtils.rm_r(["#{prepared_dir}/.git", "#{prepared_dir}/promos", "#{prepared_dir}/.gitignore", "#{prepared_dir}/prepare.rb"], :verbose => true)
+FileUtils.rm(Dir.glob("#{prepared_dir}/**/.DS_Store"), :verbose => true)
+FileUtils.rm_r(["#{prepared_dir}/.git", "#{prepared_dir}/promos", "#{prepared_dir}/.gitignore", "#{prepared_dir}/prepare.rb", "#{prepared_dir}/tests"], :verbose => true)
 
 Dir.glob("#{prepared_dir}/**/*.{js}").each do |file|
 	new_name = File.dirname(file) + "/" + File.basename(file, ".js") + "_" + version + ".js"
