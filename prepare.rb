7f3314d58bb457a3742d6ab1b0336d13450ddffa
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-10-31 11:40:00 -0400

    Replace JavaScript references in HTML and JSON files.

--- prepare.rb
+++ prepare.rb
@@ -20,4 +20,4 @@ end
 match = Regexp.escape("\\.js")
 replace = Regexp.escape("_#{version}.js")
 
-exec("find #{prepared_dir} -type f -name '*.html' -exec sed -i '' s/#{match}/#{replace}/g {} +")
+exec("find -E #{prepared_dir} -type f -iregex '.*\.(html|json)' -exec sed -i '' s/#{match}/#{replace}/g {} +")
