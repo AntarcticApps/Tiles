b74fdefe4fa9b43230bc436c7de280f0e3ecf950
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-10-31 23:33:51 -0400

    Don't show new notification on fresh install.

--- TILES_VERSION_ID_/shared/js/migrations/migrations.js
+++ TILES_VERSION_ID_/shared/js/migrations/migrations.js
@@ -4,33 +4,42 @@
 function migrateStorage() {
 	var extensionVersion = getExtensionVersion();
 
-	getStorageVersion(function(storageVersion) {
-		console.log("storage version: %s.%s.%s, extension version: %s.%s.%s",
-				storageVersion.major,
-				storageVersion.minor,
-				storageVersion.patch,
-				extensionVersion.major,
-				extensionVersion.minor,
-				extensionVersion.patch);
+	storage.get(null, function(items) {
+		var storageIsEmpty = Object.keys(items).length == 0;
 
-		if (!versionsAreEqual(extensionVersion, storageVersion)) {
-			console.log("Migration may be needed - version did change");
-			setStorageVersion(extensionVersion);
+		if (!storageIsEmpty) {
+			getStorageVersion(function(storageVersion) {
+				console.log("storage version: %s.%s.%s, extension version: %s.%s.%s",
+						storageVersion.major,
+						storageVersion.minor,
+						storageVersion.patch,
+						extensionVersion.major,
+						extensionVersion.minor,
+						extensionVersion.patch);
 
-			if (storageVersion.major == 1) {
-				if (extensionVersion.major == 2) {
-					// Migrate from a database from 1.x.x to 2.x.x
-					migrate_1_to_2(function() {
-						console.log("Migration from 1.x.x to 2.x.x complete");
-						emitMessage("refresh");
-					});
+				if (!versionsAreEqual(extensionVersion, storageVersion)) {
+					console.log("Migration may be needed - version did change");
+					setStorageVersion(extensionVersion);
+
+					if (storageVersion.major == 1) {
+						if (extensionVersion.major == 2) {
+							// Migrate from a database from 1.x.x to 2.x.x
+							migrate_1_to_2(function() {
+								console.log("Migration from 1.x.x to 2.x.x complete");
+								emitMessage("refresh");
+							});
+						}
+					}
+
+					var notification = webkitNotifications.createHTMLNotification('/TILES_VERSION_ID_/notifications/new.html');
+					notification.show();
+				} else {
+					console.log("Migration is not needed - version did not change");
 				}
-			}
-
-			var notification = webkitNotifications.createHTMLNotification('/TILES_VERSION_ID_/notifications/new.html');
-			notification.show();
+			});
 		} else {
-			console.log("Migration is not needed - version did not change");
+			console.log("Migration is not needed - new install");
+			setStorageVersion(extensionVersion);
 		}
 	});
 }
