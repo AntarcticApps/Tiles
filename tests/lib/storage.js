3b6986650ace0f64b5ac11c64959030cb8ddd284
Tanner Smith <tanner@tssoftware.net>
2012-11-18 22:57:26 -0500

    Backup the storage before each test and restore afterwards.

--- tests/lib/storage.js
+++ tests/lib/storage.js
@@ -1,11 +1,28 @@
 var ready;
+var backup;
 
 beforeEach(function() {
 	ready = false;
-	storage = TEST_STORAGE;
 
-	storage.clear(function() {
-		ready = true;
+	runs(function() {
+		if (DEFAULT_STORAGE == TEST_STORAGE) {
+			// Storage units are the same
+			storage.get(null, function(items) {
+				backup = items;
+
+				storage.clear(function() {
+					ready = true;
+				});
+			});
+			
+		} else {
+			// Storage units are not the same
+			storage = TEST_STORAGE;
+
+			storage.clear(function() {
+				ready = true;
+			});
+		}
 	});
 
 	waitsFor(function() {
@@ -14,5 +31,25 @@ beforeEach(function() {
 });
 
 afterEach(function() {
-	storage = DEFAULT_STORAGE;
+	ready = false;
+
+	runs(function() {
+		if (DEFAULT_STORAGE == TEST_STORAGE) {
+			// Storage units are the same
+			storage.clear(function() {
+				storage.set(backup, function() {
+					ready = true;
+				});
+			});
+		} else {
+			// Storage units are not the same
+			storage = DEFAULT_STORAGE;
+
+			ready = true;
+		}
+	});
+
+	waitsFor(function() {
+		return ready;
+	}, "the storage to be set up for normal use", 500);
 });
