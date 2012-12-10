716a798bd2aa8b9d642b7b4fcc7ed9cff4c8e483
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-12-09 21:33:41 -0500

    Port storage spec to relay.

--- relay/spec/storage.js
+++ relay/spec/storage.js
@@ -1,80 +1,68 @@
 describe("Storage", function() {
 	it("should be empty for each test", function() {
-		var items, ready;
+		var items;
 
-		runs(function() {
-			ready = false;
-
+		runs(function(done) {
 			storage.get(null, function(i) {
 				items = i;
-
-				ready = true;
+				done();
 			});
-		});
+		}, 500);
 
-		waitsFor(function() {
-			return ready;
-		}, "all the items in storage to be gotten", 500);
-
-		runs(function() {
+		runs(function(done) {
 			expect(items).toEqual({});
+			done();
 		});
 	});
 
 	it("should be using the test storage", function() {
 		expect(storage).toEqual(TEST_STORAGE);
-		expect(storage).toNotEqual(DEFAULT_STORAGE);
+
+		if (TEST_STORAGE != DEFAULT_STORAGE) {
+			expect(storage).toNotEqual(DEFAULT_STORAGE);
+		}
 	});
 
 	describe("version", function() {
 		it("should be able to be set and get", function() {
-			var done = false;
 			var version = null;
 
-			runs(function() {
+			runs(function(done) {
 				setStorageVersion(1261, function() {
-					done = true;
+					done();
 				});
-			});
+			}, 500);
 
-			waitsFor(function() {
-				return done;
-			}, "the storage version to be set", 500);
-
-			runs(function() {
+			runs(function(done) {
 				getStorageVersion(function(v) {
 					version = v;
+					done();
 				});
-			});
+			}, 500);
 
-			waitsFor(function() {
-				return version;
-			}, "the storage version to be gotten", 500);
-
-			runs(function() {
+			runs(function(done) {
 				expect(version).toEqual(1261);
+				done();
 			});
 		});
 
 		it("should default to 1.0.0 if it does not exist", function() {
 			var version = null;
 
-			runs(function() {
+			runs(function(done) {
 				getStorageVersion(function(v) {
 					version = v;
+					done();
 				});
-			});
+			}, 500);
 
-			waitsFor(function() {
-				return version != null;
-			}, "the storage version to be gotten", 500);
-
-			runs(function() {
+			runs(function(done) {
 				expect(version).toEqual({
 					major: 1,
 					minor: 0,
 					patch: 0
 				});
+				done();
 			});
 		});
 	});
