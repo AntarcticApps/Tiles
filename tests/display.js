1264eb8d42c881b0e16f1f3adc923a27fda35ca9
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-12-15 23:23:49 -0500

    Update Relay with timers on test page and other new Relay features.

--- tests/display.js
+++ tests/display.js
@@ -5,6 +5,8 @@ window.onload = function() {
 };
 
 var displayListener = new RelayListener();
+var timesEntered = {};
+var intervals = {};
 
 displayListener.onRelayStart = function() {
 	var parentElement = document.getElementById("relay_results");
@@ -15,7 +17,7 @@ displayListener.onRelayStart = function() {
 
 	var rootList = document.createElement("ol");
 	parentElement.appendChild(rootList);
-}
+};
 
 displayListener.onRelayEnd = function() {
 	var parentElement = document.getElementById("relay_results");
@@ -23,7 +25,7 @@ displayListener.onRelayEnd = function() {
 	var doneMessage = document.createElement("p");
 	doneMessage.innerHTML = "Relay ended.";
 	parentElement.appendChild(doneMessage);
-}
+};
 
 displayListener.onEnter = function(relayObject) {
 	if (relayObject.constructor == Describe || relayObject.constructor == It) {
@@ -44,10 +46,20 @@ displayListener.onEnter = function(relayObject) {
 		listItem.innerHTML = relayObject.name;
 		parentList.appendChild(listItem);
 		
-		var innerList = document.createElement("ol")
+		timesEntered[relayObject.id] = new Date();
+
+		var timeSpan = document.createElement("span");
+		timeSpan.setAttribute("class", "duration");
+		listItem.appendChild(timeSpan);
+
+		intervals[relayObject.id] = window.setInterval(function() {
+			timeSpan.innerHTML = timeDifference(timesEntered[relayObject.id], new Date());
+		}, 100);
+
+		var innerList = document.createElement("ol");
 		listItem.appendChild(innerList);
 	}
-}
+};
 
 displayListener.onExit = function(relayObject) {
 	if (relayObject.constructor == Expect) {
@@ -77,7 +89,48 @@ displayListener.onExit = function(relayObject) {
 			}
 			parentList.appendChild(failureReason);
 		}
+	} else if (relayObject.constructor == Describe || relayObject.constructor == It) {
+		var element = document.getElementById(relayObject.id);
+
+		clearInterval(intervals[relayObject.id]);
+		element.getElementsByClassName("duration")[0].innerHTML = timeDifference(timesEntered[relayObject.id], new Date());
 	}
+};
+
+function timeDifference(earlier, later) {
+	var difference = later.getTime() - earlier.getTime();
+	var days = Math.floor(difference / 1000 / 60 / 60 / 24);
+	difference -= days * 1000 * 60 * 60 * 24;
+	var hours = Math.floor(difference / 1000 / 60 / 60);
+	difference -= hours * 1000 * 60 * 60;
+	var minutes = Math.floor(difference / 1000 / 60);
+	difference -= minutes * 1000 * 60;
+	var seconds = Math.floor(difference / 1000);
+	difference -= seconds * 1000;
+	var milliseconds = difference;
+
+	var str = "";
+	if (days > 0){
+		str += days + " day" + (days > 1 ? "s" : "") + ", ";
+	}
+
+	if (minutes > 0){
+		str += minutes + " minute" + (minutes > 1 ? "s" : "") + ", ";
+	}
+
+	if (seconds > 0) {
+		str += seconds + " second" + (seconds > 1 ? "s" : "") + ", ";
+	}
+
+	if (milliseconds > 0) {
+		str += milliseconds + " millisecond" + (milliseconds > 1 ? "s" : "");
+	}
+
+	if (str == "") {
+		str += "0 milliseconds";
+	}
+
+	return str;
 }
 
 addRelayListener(displayListener);
